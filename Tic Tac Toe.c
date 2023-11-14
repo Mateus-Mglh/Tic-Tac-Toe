65dd5e4750321bf1a96335bafb813185f1a5a1e9
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2023-11-14 16:19:30 -0300

    making some changes

--- Tic Tac Toe.c
+++ Tic Tac Toe.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define PLAYER_WINS 1
+#define CPU_WINS 2
+
 /*16) Faça um programa para jogar o jogo da velha. Ao final imprima o resultado do jogo e pergunte 
 se deseja jogar novamente.*/
 
@@ -39,25 +42,61 @@ int main(){
     do{
       //entrada das coordenadas pelo usuário
       int xCoord, yCoord;
-      do{
+      do {
         printf("\nYour turn. Insert a x coordinate (from 0 - 2) and a y coordinate (from 0 - 2): ");
         scanf("%d %d", &xCoord, &yCoord);
-      }while(xCoord < 0 || xCoord > 2 || yCoord < 0 || yCoord > 2 || board[xCoord][yCoord] != ' ');
-      for(int i = 0; i < 3; i++)
-        for(int j = 0; j < 3; j++)
-          if(xCoord == i && yCoord == j)
-            board[xCoord][yCoord] = 'X';
+      }
+      while(xCoord < 0 || xCoord > 2 || yCoord < 0 || yCoord > 2 || board[xCoord][yCoord] != ' ');
+
+      
+      board[xCoord][yCoord] = 'X';
 
       moves++;
       printf("%d ", moves);
 
+      //verificação para linha para o jogador
+      if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X' ||
+         board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X' ||
+         board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X'){
+        victory = PLAYER_WINS;
+        playersWins++;
+        break;
+      }
+
+      //verificação para coluna para o jogador
+      else if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' ||
+         board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X' ||
+         board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X'){
+        victory = PLAYER_WINS;
+        playersWins++;
+        break;
+      }
+
+      //verificação da diagonnal principal para o jogador
+      else if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X'){
+        victory = PLAYER_WINS;
+        playersWins++;
+        break;
+      }
+
+      //verificação da diagonal secundária para o jogador
+      else if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'){
+        victory = PLAYER_WINS;
+        playersWins++;
+        break;
+      }
+
       //jogada da CPU
       int xCoord_cpu, yCoord_cpu;
+
       srand(time(NULL));
-      do{
+
+      do {
         xCoord_cpu = rand() % 3;
         yCoord_cpu = rand() % 3;
-      }while(board[xCoord_cpu][yCoord_cpu] != ' ' || board[xCoord_cpu][yCoord_cpu] == 'X');
+      } 
+      while(board[xCoord_cpu][yCoord_cpu] != ' ');
+
       board[xCoord_cpu][yCoord_cpu] = 'O';
 
       moves++;
@@ -77,83 +116,56 @@ int main(){
         printf("\n");
       }
 
-      //verificação para linha para o jogador
-      if(board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X' ||
-         board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X' ||
-         board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X'){
-        victory = 1;
-        playersWins++;
-        printf("\nPlayer wins\n");
-      }
-
-      //verificação para coluna para o jogador
-      if(board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' ||
-         board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X' ||
-         board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X'){
-        victory = 1;
-        playersWins++;
-        printf("\nPlayer wins\n");
-      }
-
-      //verificação da diagonnal principal para o jogador
-      if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X'){
-        victory = 1;
-        playersWins++;
-        printf("\nPlayer wins\n");
-      }
-
-      //verificação da diagonal secundária para o jogador
-      if(board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'){
-        victory = 1;
-        playersWins++;
-        printf("\nPlayer wins\n");
-      }
-
       //verificação para linha para a CPU
       if(board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O' ||
          board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O' ||
          board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O'){
-        victory = 1;
+        victory = CPU_WINS;
         cpuWins++;
-        printf("\nCPU wins\n");
+        break;
       }
 
       //verificação para coluna para a CPU
-      if(board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' ||
+      else if(board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' ||
          board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O' ||
          board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O'){
-        victory = 1;
+        victory = CPU_WINS;
         cpuWins++;
-        printf("\nPlayer wins\n");
+        break;
       }
 
       //verificação da diagonnal principal para a CPU
-      if(board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O'){
-        victory = 1;
+      else if(board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O'){
+        victory = CPU_WINS;
         cpuWins++;
-        printf("\nCPU wins\n");
+        break;
       }
 
       //verificação da diagonal secundária para a CPU
-      if(board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O'){
-        victory = 1;
+      else if(board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+        victory = CPU_WINS;
         cpuWins++;
-        printf("\nCPU wins\n");
+        break;
       }
 
-    }while(victory < 1 && moves < 9);
+    } while(victory < 1 && moves < 9);
 
-    if(victory == 0)
+    if(!victory) {
       printf("Draw\n");
+    } else if(victory == PLAYER_WINS) {
+        printf("\nPLAYER WINS!!\n\n");
+    } else {
+        printf("\nCPU WINS\n\n");
+    }
 
     printf("\nScore\nPlayer: %d\tCPU: %d\n", playersWins, cpuWins);
 
-    getchar();
+    while(getchar() != '\n');
     printf("\nWanna play again? (y/n) ");
     scanf("%c", &option);
 
     printf("\n");
-  }while(option == 'y' && option != 'n');
+  } while(option == 'y' && option != 'n');
 
   return 0;
 }
